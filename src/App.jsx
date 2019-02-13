33f52d25759121e193c372fc647ccc1ea448379f
Tony Kieling <tony.kieling@gmail.com>
2019-02-13 03:44:49 +0000

    data flow and showing hard-coded messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import React, {Component} from 'react';
+// import { generateRandomId } from "./utils";
 
 function Welcome(props) {
   return <h1 className="navbar">Chatty</h1>;
 }
 
 
-class Messages extends Component {
-  // constructor
+class MessageList extends Component {
   render() {
+    let allMessages = this.props.messages.map((message) => {
+      return <Message key={message.id} message={message} />
+    });
+    return (
+      <div className="messages">
+        {allMessages}
+      </div>
+    )
+  }
+}
+
+
+class Message extends Component {
+  render() {
+    console.log("this: ", this.props);
     return (
       <div>
         <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+          <span className="message-username">{this.props.message.userName}</span>
+          <span className="message-content">{this.props.message.content}</span>
         </div>
         <div className="message system">
-          Anonymous1 changed their name to nomnom.
         </div>
       </div>
     );
@@ -23,11 +37,11 @@ class Messages extends Component {
 }
 
 
-class Chatbr extends Component {
+class Chatbar extends Component {
   render () {
     return (
       <div className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username"  defaultValue={this.props.user.name}/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </div>
     )
@@ -40,15 +54,27 @@ class App extends Component {
     super(props);
 
     this.state = {
-
-    }
+      currentUser: {name: "Bob"},
+      messages: [
+        {
+          userName: "Bob",
+          content: "Has anyone seen my marbles?",
+          id:"11"
+        },
+        {
+          userName: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id:"21"
+        }
+      ]};
   }
+
   render() {
     return (
       <div>
         <Welcome />
-        <Messages />
-        <Chatbr />
+        <MessageList messages={this.state.messages}/>
+        <Chatbar user={this.state.currentUser}/>
       </div>
     );
   }
