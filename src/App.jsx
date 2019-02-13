298911bacb593a4c618a18a63deec26374cd43b8
Tony Kieling <tony.kieling@gmail.com>
2019-02-13 06:22:47 +0000

    done but missing onKeyDown..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 // import { generateRandomId } from "./utils";
+// import keydown from 'react-keydown';
 
 function Welcome(props) {
   return <h1 className="navbar">Chatty</h1>;
@@ -38,11 +39,55 @@ class Message extends Component {
 
 
 class Chatbar extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.callAddMsg = this.callAddMsg.bind(this);
+  //   // this.checkEnter = this.checkEnter.bind(this);
+  // }
+
+  // checkEnter(event) {
+    // event.prevenDefault;
+  // checkEnter = (event) => {
+    // event.preventDefault();
+  // onKeyPress = (e) => {    
+  //   console.log("ENTER");
+  //   // (event.keyCode === 13) ? this.props.addMessage : ""
+  // }
+
+  // handleKeyPress = (event) => {
+  //   console.log("wwwwwwwwwwww");
+  //   if(event.key === "13") {
+  //     console.log("+++++++++++");
+  //   }
+  // } 
+  // onKeyDown={ this.myMethod }
+
+
+  // callAddMsg(event) {
+  //   console.log("event.username: ", event.userName);
+  //   event.preventDefault();
+  //   this.props.addMessage(event);
+  // }
+
+  
+
   render () {
+    const onSubmit = evt => {
+      evt.preventDefault();
+      // console.log("evt.target.elements.content: ", evt.target.elements.userName.value);
+      const message = evt.target.elements;
+      this.props.addMessage(message);
+      evt.target.elements.content.value = "";
+    };
+
     return (
       <div className="chatbar">
-        <input className="chatbar-username"  defaultValue={this.props.user.name}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <form onSubmit={onSubmit}>
+          <input className="chatbar-username" type="text" name="userName"
+            defaultValue={this.props.user.name}  />
+          <input className="chatbar-message" placeholder="Type a message and hit ENTER" name="content"/>
+          <button>Send Msg</button>
+        </form>
       </div>
     )
   }
@@ -59,14 +104,17 @@ class App extends Component {
         {
           userName: "Bob",
           content: "Has anyone seen my marbles?",
-          id:"11"
+          id: 1
         },
         {
           userName: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-          id:"21"
+          id: 2
         }
       ]};
+    
+    this.addMessage = this.addMessage.bind(this);
+      
   }
 
   componentDidMount() {
@@ -79,7 +127,22 @@ class App extends Component {
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
       this.setState({messages: messages})
-    }, 3000);
+    }, 2000);
+  }
+
+  addMessage(data) {
+    console.log("this.state::::: ", this.state);
+    const oldMsgs = this.state.messages;
+    const tempMsg = {
+      userName: data.userName.value,
+      content: data.content.value,
+      id: this.state.messages.length + 1
+    };
+    console.log("oldMsgs: ", oldMsgs);
+    const newMsg = [...oldMsgs, tempMsg];
+
+    console.log("newMsgs: ", newMsg);
+    this.setState({ messages: newMsg });
   }
 
   render() {
@@ -87,7 +150,7 @@ class App extends Component {
       <div>
         <Welcome />
         <MessageList messages={this.state.messages}/>
-        <Chatbar user={this.state.currentUser}/>
+        <Chatbar user={this.state.currentUser} addMessage={this.addMessage} />
       </div>
     );
   }
